94cebfabeaf1c5c98aa02c4f427afc483496187f
bobits <bobkeri.gabule@gmail.com>
2025-06-19 02:07:25 +0800

    add schadcn & sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,38 @@
-const Home = () => {
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger
+} from "@/components/ui/sidebar"
+import AppSidebarClient from "./_AppsidebarClient"
+
+const HomePage = () => {
   return (
-    <div>Home</div>
+    <SidebarProvider className="overflow-y-hidden">
+      <AppSidebarClient>
+        <Sidebar collapsible="icon" className="overflow-hidden">
+          <SidebarHeader className="flex-row">
+            <SidebarTrigger />
+              <span className="text-xl text-nowrap">Next Jobs</span>
+          </SidebarHeader>
+          <SidebarContent></SidebarContent>
+          <SidebarFooter>
+            <SidebarMenu>
+              <SidebarMenuItem>
+                <SidebarMenuButton>adasd</SidebarMenuButton>
+              </SidebarMenuItem>
+            </SidebarMenu>
+          </SidebarFooter>
+        </Sidebar>
+        <main className="flex-1">asdasdsad</main>
+      </AppSidebarClient>
+    </SidebarProvider>
   )
 }
 
-export default Home
+export default HomePage
