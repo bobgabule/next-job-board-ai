e8be4958961328c243d2c0da5080fa40e268c44e
bobits <bobkeri.gabule@gmail.com>
2025-06-19 01:23:38 +0800

    setup database & schema

---
+++ src/drizzle/schema.ts
@@ -0,0 +1,7 @@
+export * from "./schema/user"
+export * from "./schema/organization"
+export * from "./schema/jobListing"
+export * from "./schema/userResume"
+export * from "./schema/jobListingApplication"
+export * from "./schema/organizationUserSettings"
+export * from "./schema/userNotificationSettings"
